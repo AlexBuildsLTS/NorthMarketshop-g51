647984e476e7fc5ccfc31f9cbbd5f50122f89024
Alex Youssef <Alex.youssef@live.com>
2024-11-14 11:53:53 +0200

    Initial commit project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to NorthMarketX!</h1>
+      {/* We will add more components here later, like SignIn, SignUp, Dashboard */}
+    </div>
+  );
+}
+
+export default App;
